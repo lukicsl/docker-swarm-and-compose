3d2aa8dcc4df5d73b808db8e1faab2f2cb75329f
lukicsl <lukic@gmx.ch>
2019-02-04 10:44:49 +0100

    Update and rename start-stack.sh to start-stacks.sh

--- start-stacks.sh
+++ start-stacks.sh
@@ -1,3 +1,4 @@
+docker stack deploy -c docker-flow/docker-flow-proxy.yml proxy
 docker stack deploy -c LMS/sb-server.yml sb-server
 docker stack deploy -c grafana/grafana.yml grafana
 docker stack deploy -c influxdb/influxdb.yml influxdb
