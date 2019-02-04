169862507c1ce1eeb95bc7bbf6bbf5978d3db381
lukicsl <lukic@gmx.ch>
2019-02-04 10:39:15 +0100

    Create start-stack.sh

---
+++ start-stack.sh
@@ -0,0 +1,6 @@
+docker stack deploy -c LMS/sb-server.yml sb-server
+docker stack deploy -c grafana/grafana.yml grafana
+docker stack deploy -c influxdb/influxdb.yml influxdb
+docker stack deploy -c mosquitto/mqtt.yml mosquitto
+docker stack deploy -c ebusd/ebusd.yml ebusd
+docker stack deploy -c openhab/openhab-2.4.0.yml openhab
