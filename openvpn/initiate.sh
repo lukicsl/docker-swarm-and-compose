ff3e0c53aa9b86bbf194a4d51eb990afce6990c7
lukicsl <lukic@gmx.ch>
2019-02-07 16:44:44 +0100

    Update initiate.sh

--- openvpn/initiate.sh
+++ openvpn/initiate.sh
@@ -1,9 +1,9 @@
 # setup server
-docker run -v /datavol/openvpn/etc:/etc/openvpn --log-driver=none --rm lukics/openvpn:armv7 ovpn_genconfig -u udp://lukic.ch:3000
+docker run -v /datavol/openvpn/etc:/etc/openvpn --log-driver=none --rm lukics/openvpn:armv7 ovpn_genconfig -u udp://lukic.ch:1194
 docker run -v /datavol/openvpn/etc:/etc/openvpn --log-driver=none --rm -it lukics/openvpn:armv7 ovpn_initpki
 
 # run server
-docker run -v /datavol/openvpn/etc:/etc/openvpn -d -p 3000:3000/udp --cap-add=NET_ADMIN lukics/openvpn:armv7
+docker run -v /datavol/openvpn/etc:/etc/openvpn -d -p 1194:1194/udp --cap-add=NET_ADMIN lukics/openvpn:armv7
 
 # create user, extract cerificate
 docker run -v /datavol/openvpn/etc:/etc/openvpn --log-driver=none --rm -it lukics/openvpn:armv7 easyrsa build-client-full lukics nopass
