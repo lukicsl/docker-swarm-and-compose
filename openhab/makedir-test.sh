510913854d0c0e3c828c57e0b932b8e93ea6c559
lukicsl <lukic@gmx.ch>
2019-02-13 08:30:09 +0100

    Create makedir-test.sh

---
+++ openhab/makedir-test.sh
@@ -0,0 +1,7 @@
+mkdir -p /datavol/openhab-2.4.0/openhab_addons
+mkdir -p /datavol/openhab-2.4.0/openhab_conf
+mkdir -p /datavol/openhab-2.4.0/openhab_userdata
+
+mkdir -p /datavol/openhab-2.5.0/openhab_addons
+mkdir -p /datavol/openhab-2.5.0/openhab_conf
+mkdir -p /datavol/openhab-2.5.0/openhab_userdata
