a6fb4a1b6703d670036162f77a62063a2a1396c3
lukicsl <lukic@gmx.ch>
2019-02-21 18:19:53 +0100

    Update start-stacks.sh

--- start-stacks.sh
+++ start-stacks.sh
@@ -4,4 +4,5 @@ docker stack deploy -c grafana/grafana.yml grafana
 docker stack deploy -c influxdb/influxdb.yml influxdb
 docker stack deploy -c mosquitto/mqtt.yml mosquitto
 docker stack deploy -c ebusd/ebusd.yml ebusd
+docker stack deploy -c landroid/landroid.yml landroid
 docker stack deploy -c openhab/openhab-2.4.0.yml openhab
