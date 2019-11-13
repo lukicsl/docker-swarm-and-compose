ed381609de04ae281c0bb0aa76a5799503f24a54
lukicsl <lukic@gmx.ch>
2019-11-13 11:32:33 +0100

    Update start-docker-compose.sh

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -7,6 +7,6 @@ docker-compose -f  ebusd/docker-compose.yml up -d
 docker-compose -f  landroid/docker-compose.yml up -d
 export HOSTNAME
 export OH_VERSION="2.5.0.M2"
-docker-compose -f openhab-all-compose.yml up -d
+docker-compose -f openhab/openhab-all-compose.yml up -d
 docker ps
  
