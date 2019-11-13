fe70b848e4a0cabc7673e592c41b3b1faf7ee452
lukicsl <lukic@gmx.ch>
2019-11-13 11:21:54 +0100

    Create start-docker-compose.sh

---
+++ start-docker-compose.sh
@@ -0,0 +1,9 @@
+docker-compose -f  sbserver/docker-compose.yml up -d
+docker-compose -f  grafana/docker-compose.yml up -d
+docker-compose -f  influxdb/docker-compose.yml up -d
+docker-compose -f  mosquitto/docker-compose.yml up -d
+docker-compose -f  ebusd/docker-compose.yml up -d
+docker-compose -f  landroid/docker-compose.yml up -d
+export HOSTNAME
+export OH_VERSION="2.5.0.M2"
+docker-compose -f openhab-all-compose.yml up -d
