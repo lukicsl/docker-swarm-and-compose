e58ebba82d398f8df9fa8c68f75204cfb5b8b1da
lukicsl <lukic@gmx.ch>
2019-11-13 11:28:56 +0100

    Update start-docker-compose.sh

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -1,3 +1,4 @@
+docker-compose -f  openvpn/docker-compose.yml up -d
 docker-compose -f  sbserver/docker-compose.yml up -d
 docker-compose -f  grafana/docker-compose.yml up -d
 docker-compose -f  influxdb/docker-compose.yml up -d
